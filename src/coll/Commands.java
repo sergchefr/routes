577a9fd7e4cfe659d45a4ca3c19376672f00001d
sergchefr <serg.chefr@gmail.com>
2025-02-17 00:42:28 +0300

    pre-com

---
+++ src/coll/Commands.java
@@ -0,0 +1,21 @@
+package coll;
+
+import java.util.Arrays;
+/** Интерфейс с командами управления коллекцией*/
+public interface Commands {
+    public String save(String filename);
+    public String load(String filename);
+    public String add(Route route);
+    public String info();
+    public String show();
+    public String update(int id, Route route);
+    public String removeById(int id);
+    public String clear();
+    public String executeScript(String fileName);
+    public String exit();
+    public String addIfMax(Route route);
+    public String addIfMin(Route route);
+    public String avgdistance();
+    public String printAsc();
+    public String printAscDist();
+}
